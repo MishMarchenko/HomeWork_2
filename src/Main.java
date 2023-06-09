08932123bb04ff7cc2ed9ea244eb6e94d063b795
MishMarchenko <mishmar4enko@gmail.com>
2023-06-09 22:33:10 +0300

    Lust Part

--- src/Main.java
+++ src/Main.java
@@ -17,5 +17,32 @@ public class Main {
             }
             System.out.println();
         }
+        System.out.println("Let's count to 10 using While Loop");
+        int a = 0;
+        while(a < 10){
+            a = a + 1;
+            System.out.println(a);
+        }
+        System.out.println("Let's count from 10 to 1 using Do-While Loop");
+        int b = 10;
+        do{
+            System.out.println(b);
+            b = b - 1;
+        } while(b > 0);
+
+        System.out.println();
+
+        int [] [] multyArray = new int[8][9];
+        for(int c = 0; c < multyArray.length; c++){
+            for(int d = 0; d < multyArray[0].length; d++){
+                multyArray[c][d] = c + d;
+            }
+        }
+        for(int c = 0; c < multyArray.length; c++){
+            for(int d = 0; d < multyArray[0].length; d++){
+                System.out.println(multyArray[c][d]);
+            }
+            System.out.println();
+        }
     }
 }
