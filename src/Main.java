efad1188088e60145d2bdacc2824569f2808947f
MishMarchenko <mishmar4enko@gmail.com>
2023-06-09 17:01:03 +0300

    first part HW

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello Dmitriy!");
+        System.out.println("Hello Dmitriy!");
+        Scanner scanner = new Scanner(System.in);
+        int [] array = new int[scanner.nextInt()];
+        for (int a = 0; a < array.length; a++) {
+            array[a] = a + 1;
+        }
+            System.out.println(Arrays.toString(array));
+
+
+
     }
 }
