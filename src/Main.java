98682561a05dc8eaafc5c80dcacacbdd05edb41d
MishMarchenko <mishmar4enko@gmail.com>
2023-06-09 23:17:10 +0300

    Some changes in HW

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Dmitriy!");
+        System.out.println("Hello Dmitriy! Check the required part of the homework!");
+
+        System.out.println("Enter any number of array cells!");
         Scanner scanner = new Scanner(System.in);
         int [] array = new int[scanner.nextInt()];
         for (int a = 0; a < array.length; a++) {
             array[a] = a + 1;
         }
-            System.out.println(Arrays.toString(array));
+        System.out.println("Completed them in order");
+        System.out.println(Arrays.toString(array));
 
+        System.out.println();
+
+        System.out.println("Let me show you the multiplication table in reverse:");
         for (int b = 10; b > 0; b--){
             for (int c = 10; c > 0; c--) {
                 System.out.println(b + "*" + c + "=" + b*c);
@@ -32,6 +38,7 @@ public class Main {
 
         System.out.println();
 
+        System.out.println("Let's fill the multi array with data");
         int [] [] multyArray = new int[8][9];
         for(int c = 0; c < multyArray.length; c++){
             for(int d = 0; d < multyArray[0].length; d++){
